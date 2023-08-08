5fe2830be0aad7beeb2330688c3076f58953ef6d
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2023-08-08 16:31:50 +0400

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import "./Newsletter.css";
-
 function Newsletter() {
+  const [email, setEmail] = useState("");
+  const { pathname } = useLocation();
+
+  function handleEmail(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setEmail("");
+  }
+
+  useEffect(() => {
+    setEmail("");
+  }, [pathname]);
+
   return (
     <section className="newsletter">
       <h1>Newsletter</h1>
-      <form>
-        <input type="email" placeholder="your@email.com" />
+      <form onSubmit={handleEmail}>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          placeholder="your@email.com"
+        />
         <button>Subscribe</button>
       </form>
     </section>
